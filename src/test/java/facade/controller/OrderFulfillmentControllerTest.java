794410b761bc8b069cf62d8d28a6cd98c807fe92
Michal Gebarowski <vmaaik@gmail.com>
2018-08-06 09:50:32 +0100

    init test;

---
+++ src/test/java/facade/controller/OrderFulfillmentControllerTest.java
@@ -0,0 +1,25 @@
+package facade.controller;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Michał Gębarowski on 5/08/2018
+ */
+public class OrderFulfillmentControllerTest {
+
+    private final int productId = 012323;
+    private OrderFulfillmentController controller;
+
+    @BeforeAll
+    public void create() {
+        controller = new OrderFulfillmentController();
+    }
+
+    @Test
+    public void createOrder() {
+
+
+    }
+
+}
